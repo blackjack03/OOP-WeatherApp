63c221e199bb5cf03d130fdf06c187c7b33c1fbb
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-11-14 22:12:42 +0100

    Created Main.java class to run some tests

---
+++ src/main/java/org/app/travelmode/MainTest.java
@@ -0,0 +1,10 @@
+package org.app.travelmode;
+
+import javafx.application.Application;
+import org.app.travelmode.controller.TravelModeControllerImpl;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Application.launch(TravelModeControllerImpl.class, args);
+    }
+}
