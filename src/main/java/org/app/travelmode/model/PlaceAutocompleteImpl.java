8a2ba3ab9bf8531abf256aa5b75352109294ce55
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-11-20 19:00:19 +0100

    Created PlaceAutocompleteImpl.java file and provided first implementation

---
+++ src/main/java/org/app/travelmode/model/PlaceAutocompleteImpl.java
@@ -0,0 +1,15 @@
+package org.app.travelmode.model;
+
+import java.util.List;
+
+public class PlaceAutocompleteImpl implements PlaceAutocomplete {
+
+    public PlaceAutocompleteImpl() {
+
+    }
+
+    public List<String> getSuggestion(final String input){
+        return null;
+    }
+
+}
