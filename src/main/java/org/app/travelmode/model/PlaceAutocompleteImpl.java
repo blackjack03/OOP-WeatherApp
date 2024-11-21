f84c6d5d3f7ccf56e4d7477387b5e256abac2490
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-11-21 22:54:20 +0100

    Changed PlaceAutocompleteImpl: can now get google api key from json file

--- src/main/java/org/app/travelmode/model/PlaceAutocompleteImpl.java
+++ src/main/java/org/app/travelmode/model/PlaceAutocompleteImpl.java
@@ -1,14 +1,28 @@
 package org.app.travelmode.model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class PlaceAutocompleteImpl implements PlaceAutocomplete {
 
-    public PlaceAutocompleteImpl() {
+    private final String googleApiKey;
 
+    public PlaceAutocompleteImpl() {
+        final String[] inputLine;
+        try (final BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/resources/API-Keys.json")))) {
+            br.readLine();
+            inputLine = br.readLine().split(":");
+            this.googleApiKey = inputLine[1];
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(googleApiKey);
     }
 
-    public List<String> getSuggestion(final String input){
+    public List<String> getSuggestion(final String input) {
         return null;
     }
 
