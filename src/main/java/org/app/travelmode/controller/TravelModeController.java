af873b1e1e1be9cc471c6ba57ad81d19af867f24
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-11-25 16:51:28 +0100

    Modified TravelModeController and TravelModeControllerImpl: Added getPlacePredictions method

--- src/main/java/org/app/travelmode/controller/TravelModeController.java
+++ src/main/java/org/app/travelmode/controller/TravelModeController.java
@@ -1,5 +1,16 @@
 package org.app.travelmode.controller;
 
+import org.app.travelmode.placeautocomplete.PlaceAutocompletePrediction;
+
+import java.util.List;
+
 public interface TravelModeController {
 
+    /**
+     * Provides a list of {@link PlaceAutocompletePrediction} based on the input text
+     *
+     * @param input String representing the characters written by the user
+     * @return a list of predictions based on the input
+     */
+    List<PlaceAutocompletePrediction> getPlacePredictions(final String input);
 }
--- src/main/java/org/app/travelmode/controller/TravelModeControllerImpl.java
+++ src/main/java/org/app/travelmode/controller/TravelModeControllerImpl.java
@@ -2,15 +2,22 @@ package org.app.travelmode.controller;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.util.List;
+import org.app.travelmode.placeautocomplete.PlaceAutocompletePrediction;
 import org.app.travelmode.view.TravelModeView;
 import org.app.travelmode.view.TravelModeViewImpl;
 
 public class TravelModeControllerImpl extends Application implements TravelModeController {
 
-    private final TravelModeView view = new TravelModeViewImpl();
+    private final TravelModeView view = new TravelModeViewImpl(this);
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
         this.view.start();
+    }
+
+    @Override
+    public List<PlaceAutocompletePrediction> getPlacePredictions(final String input) {
+        return null;
     }
 }
