db2f4f3598cb7955f009f21b373c7755ed60da5b
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-11-27 17:26:57 +0100

    Created TravelRequest.java and provided first implementation

---
+++ src/main/java/org/app/travelmode/model/TravelRequest.java
@@ -0,0 +1,50 @@
+package org.app.travelmode.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public interface TravelRequest {
+
+    /**
+     * Allows you to obtain the name of the departure location stored in this request
+     *
+     * @return the departure city stored in this request
+     */
+    String getDepartureLocation();
+
+    /**
+     * Allows you to obtain the PlaceId corresponding to the departure location
+     *
+     * @return the PlaceId corresponding to the departure location
+     */
+    String getDepartureLocationPlaceId();
+
+    /**
+     * Allows you to get the name of the arrival location stored in this request
+     *
+     * @return the name of the arrival location stored in this request
+     */
+    String getArrivalLocation();
+
+    /**
+     * Allows you to obtain the PlaceId corresponding to the arrival location
+     *
+     * @return the PlaceId corresponding to the arrival location
+     */
+    String getArrivalLocationPlaceId();
+
+    /**
+     * Allows you to get the departure time stored in this request
+     *
+     * @return the departure time stored in this request
+     */
+    LocalTime getDepartureTime();
+
+    /**
+     * Allows you to get the departure date stored in this request
+     *
+     * @return the departure date stored in this request
+     */
+    LocalDate getDepartureDate();
+
+}
