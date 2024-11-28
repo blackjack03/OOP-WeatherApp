b15b80bcca17b550044d6f145af76501d66f410b
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-11-28 15:53:15 +0100

    Modified TravelModeController: added the necessary methods for configuring a TrvelRequest

--- src/main/java/org/app/travelmode/controller/TravelModeController.java
+++ src/main/java/org/app/travelmode/controller/TravelModeController.java
@@ -1,7 +1,10 @@
 package org.app.travelmode.controller;
 
+import org.app.travelmode.model.TravelRequestImpl;
 import org.app.travelmode.placeautocomplete.PlaceAutocompletePrediction;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface TravelModeController {
@@ -13,4 +16,57 @@ public interface TravelModeController {
      * @return a list of predictions based on the input
      */
     List<PlaceAutocompletePrediction> getPlacePredictions(final String input);
+
+    /**
+     * Set the departure location
+     *
+     * @param departureLocation the name of the departure location
+     * @return this builder, for method chaining
+     */
+    TravelRequestImpl.TravelRequestBuilder setDepartureLocation(final String departureLocation);
+
+    /**
+     * Set the PlaceId associated with the starting location
+     *
+     * @param departurePlaceId the PlaceId associated with the starting location
+     * @return this builder, for method chaining
+     */
+    TravelRequestImpl.TravelRequestBuilder setDeparturePlaceId(final String departurePlaceId);
+
+    /**
+     * Set the arrival location
+     *
+     * @param arrivalLocation the name of the arrival location
+     * @return this builder, for method chaining
+     */
+    TravelRequestImpl.TravelRequestBuilder setArrivalLocation(final String arrivalLocation);
+
+    /**
+     * Set the PlaceId associated with the arrival location
+     *
+     * @param arrivalPlaceId the PlaceId associated with the arrival location
+     * @return this builder, for method chaining
+     */
+    TravelRequestImpl.TravelRequestBuilder setArrivalPlaceId(final String arrivalPlaceId);
+
+    /**
+     * Set the departure time
+     *
+     * @param departureTime the departure time
+     * @return this builder, for method chaining
+     */
+    TravelRequestImpl.TravelRequestBuilder setDepartureTime(final LocalTime departureTime);
+
+    /**
+     * Set the departure date
+     *
+     * @param departureDate the departure date
+     * @return this builder, for method chaining
+     */
+    TravelRequestImpl.TravelRequestBuilder setDepartureDate(final LocalDate departureDate);
+
+    /**
+     * @return a TravelRequestImpl
+     */
+    TravelRequestImpl build();
 }
