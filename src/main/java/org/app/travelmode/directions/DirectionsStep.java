82aef8ef9fa3bca5c050c5f34fb21d72f63912f7
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-12-03 16:06:50 +0100

    Modified DirectionsStep: Now extends the SimpleDirectionsStep class to take advantage of inheritance

--- src/main/java/org/app/travelmode/directions/DirectionsStep.java
+++ src/main/java/org/app/travelmode/directions/DirectionsStep.java
@@ -4,60 +4,34 @@ package org.app.travelmode.directions;
  * Each element in the steps of a {@link DirectionsLeg} defines a single step of the calculated directions.
  * A step is the most atomic unit of a direction's route, containing a single step describing a specific, single instruction on the journey.
  */
-public class DirectionsStep {
+public class DirectionsStep extends SimpleDirectionsStep {
 
-    private TextValueObject duration;
-    private LatLng end_location;
-    private DirectionsPolyline polyline;
-    private LatLng start_location;
-    private String travel_mode;
-    private TextValueObject distance;
-
-    public DirectionsStep(final TextValueObject duration, final LatLng end_location, final LatLng start_location, final TextValueObject distance) {
-        this.duration = duration;
-        this.end_location = end_location;
-        this.start_location = start_location;
-        this.distance = distance;
-    }
+    private final DirectionsPolyline polyline;
+    private final String travel_mode;
 
     public DirectionsStep(final TextValueObject duration, final LatLng end_location, final LatLng start_location,
                           final TextValueObject distance, final String travel_mode, final DirectionsPolyline polyline) {
-        this(duration, end_location, start_location, distance);
+        super(duration, end_location, start_location, distance);
         this.travel_mode = travel_mode;
         this.polyline = polyline;
-    }
-
-    public TextValueObject getDuration() {
-        return this.duration;
-    }
-
-    public LatLng getEnd_location() {
-        return this.end_location;
     }
 
     public DirectionsPolyline getPolyline() {
         return this.polyline;
     }
 
-    public LatLng getStart_location() {
-        return this.start_location;
-    }
-
     public String getTravel_mode() {
         return this.travel_mode;
     }
 
-    public TextValueObject getDistance() {
-        return this.distance;
+    public String toStringBasic() {
+        return super.toString();
     }
 
     @Override
     public String toString() {
-        return "DirectionsStep{" + "duration= " + duration +
-                ",\ndistance= " + distance +
-                ",\nstart_location= " + start_location +
-                ",\nend_location= " + end_location +
-                ",\npolyline= " + polyline +
-                ",\ntravel_mode= " + travel_mode;
+        return "DirectionsStep{" + "[duration= " + super.getDuration() + "], [distance= " + super.getDistance()
+                + "], start_location= " + super.getStart_location() + "], [end_location= " + super.getEnd_location()
+                + "], [polyline= " + polyline + "], [travel_mode= " + travel_mode + "]}";
     }
 }
