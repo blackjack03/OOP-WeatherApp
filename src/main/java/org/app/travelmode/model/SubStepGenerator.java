72521c6ae4c345a03100c1bbf65e8c64cab762e0
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-12-08 20:10:42 +0100

    Modified SubStepGenerator: added javadoc

--- src/main/java/org/app/travelmode/model/SubStepGenerator.java
+++ src/main/java/org/app/travelmode/model/SubStepGenerator.java
@@ -7,5 +7,12 @@ import java.util.List;
 
 public interface SubStepGenerator {
 
+    /**
+     * Generates sub-steps for a given {@link DirectionsStep}.
+     *
+     * @param step the step to process
+     * @return a list of sub-steps represented as {@link SimpleDirectionsStep} objects
+     */
     List<SimpleDirectionsStep> generateSubSteps(DirectionsStep step);
+
 }
