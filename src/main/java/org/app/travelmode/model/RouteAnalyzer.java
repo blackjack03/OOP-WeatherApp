ac841c5717e1386156cb94c459c8666430eeb412
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-12-08 19:11:34 +0100

    Changed javadoc in RouteAnalyzer.

--- src/main/java/org/app/travelmode/model/RouteAnalyzer.java
+++ src/main/java/org/app/travelmode/model/RouteAnalyzer.java
@@ -8,10 +8,10 @@ import java.util.List;
 public interface RouteAnalyzer {
 
     /**
-     * Group all the steps of an entire {@link DirectionsRoute} into {@link SimpleDirectionsStep}. Each of these will be approximately equal in length.
+     * Calculates the intermediate points along the provided route. The distance between each intermediate point will be approximately equal.
      *
-     * @param directionsRoute represents the path
-     * @return A list of SimpleDirectionsStep each of which will be approximately equal in length.
+     * @param directionsRoute the route to analyze
+     * @return A list of intermediate points represented as {@link SimpleDirectionsStep} objects
      */
     List<SimpleDirectionsStep> calculateIntermediatePoints(DirectionsRoute directionsRoute);
 
