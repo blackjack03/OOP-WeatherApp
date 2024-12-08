089e3ffeef39f460a471fce0086e15b5a7ac3e85
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-12-08 20:06:59 +0100

    Modified IntermediatePointFinder: added javadoc

--- src/main/java/org/app/travelmode/model/IntermediatePointFinder.java
+++ src/main/java/org/app/travelmode/model/IntermediatePointFinder.java
@@ -7,5 +7,12 @@ import java.util.List;
 
 public interface IntermediatePointFinder {
 
+    /**
+     * Finds intermediate points for a given route leg.
+     *
+     * @param directionsLeg the {@link DirectionsLeg} to analyze
+     * @param subStepGenerator the generator for creating sub-steps from a DirectionsStep
+     * @return a list of intermediate points as {@link SimpleDirectionsStep} objects
+     */
     List<SimpleDirectionsStep> findIntermediatePoints(DirectionsLeg directionsLeg, SubStepGenerator subStepGenerator);
 }
