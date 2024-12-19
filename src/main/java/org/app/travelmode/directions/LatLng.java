b4579f9e6af95b2297ae213885783c7fff416bd8
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-12-19 16:42:42 +0100

    Modified LatLng: added hasCode and equals methods

--- src/main/java/org/app/travelmode/directions/LatLng.java
+++ src/main/java/org/app/travelmode/directions/LatLng.java
@@ -1,5 +1,7 @@
 package org.app.travelmode.directions;
 
+import java.util.Objects;
+
 /**
  * A place on Earth, represented by a Latitude/Longitude pair.
  */
@@ -10,6 +12,7 @@ public class LatLng {
 
     /**
      * Construct a location with a latitude longitude pair.
+     *
      * @param lat latitude
      * @param lng longitude
      */
@@ -24,6 +27,27 @@ public class LatLng {
 
     public double getLng() {
         return this.lng;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lat, lng);
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LatLng tmp = (LatLng) obj;
+        return Objects.equals(lat, tmp.lat) && Objects.equals(lng, tmp.lng);
     }
 
     @Override
