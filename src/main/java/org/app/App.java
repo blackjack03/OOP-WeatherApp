eae2d605793982e1c20e51f0a9ac252a2b6fecc8
Tommaso Valeriani <tommaso.valeriani@studio.unibo.it>
2025-01-11 17:23:31 +0100

    Updated App.java

--- src/main/java/org/app/App.java
+++ src/main/java/org/app/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +12,17 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/app/view/mainview.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/mainview.fxml"));
         Parent root = loader.load();
 
-        Scene scene = new Scene(root);
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+        primaryStage.setMinHeight(900);
+        primaryStage.setMinWidth(1750);
+
+        Scene scene = new Scene(root, screenWidth * 0.8, screenHeight * 0.8);
+        //Scene scene = new Scene(root);
         primaryStage.setTitle("Weather Application");
         primaryStage.setScene(scene);
         primaryStage.show();
