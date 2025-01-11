700a85faccce43c37f273e86af74c324dff9f753
Tommaso Valeriani <tommaso.valeriani@studio.unibo.it>
2025-01-11 17:20:59 +0100

    Updated mainGUIcontroller with elements to test the GUI

--- src/main/java/org/app/controller/mainGUIcontroller.java
+++ src/main/java/org/app/controller/mainGUIcontroller.java
@@ -1,31 +1,68 @@
 package org.app.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.*;
 import javafx.scene.control.*;
 import javafx.beans.property.*;
 
 public class mainGUIcontroller {
     @FXML
-    private Label temperatureLabel;
+    private Label currentStatusLabel;
+    
+    @FXML
+    private Label currentTemperatureLabel;
 
-    private StringProperty temperature = new SimpleStringProperty("20°C");
+    @FXML
+    private Label currentPerceivedTemperatureLabel;
+
+    @FXML
+    private ImageView currentWeatherImage;
+
+    @FXML
+    private Label currentMaxTemperatureLabel;
+
+    @FXML
+    private Label currentMinTemperatureLabel;
+
+    @FXML
+    private Button settingsButton;
+
+    private StringProperty currentStatusTemperature = new SimpleStringProperty("Soleggiato");
+    private StringProperty currentTemperature = new SimpleStringProperty("Temperatura: 20°C");
+    private StringProperty currentPerceivedTemperature = new SimpleStringProperty("Percepita: 18°C");
+    private StringProperty currentMaxTemperature = new SimpleStringProperty("Max: 15°C");
+    private StringProperty currentMinTemperature = new SimpleStringProperty("Min: 5°C");
+
 
     public StringProperty temperatureProperty() {
-        return temperature;
+        return currentTemperature;
     }
 
     public void setTemperature(String temp) {
-        temperature.set(temp);
+        currentTemperature.set(temp);
     }
 
     public String getTemperature() {
-        return temperature.get();
+        return currentTemperature.get();
     }
 
     @FXML
     public void initialize() {
         // Binding della proprietà con la Label
-        temperatureLabel.textProperty().bind(temperature);
+        currentStatusLabel.textProperty().bind(currentStatusTemperature);
+        currentTemperatureLabel.textProperty().bind(currentTemperature);
+        currentPerceivedTemperatureLabel.textProperty().bind(currentPerceivedTemperature);
+        currentMaxTemperatureLabel.textProperty().bind(currentMaxTemperature);
+        currentMinTemperatureLabel.textProperty().bind(currentMinTemperature);
+
+        Image image = new Image(getClass().getResource("/images/0.png").toExternalForm());
+        currentWeatherImage.setImage(image);
+        Image settingsButtonImage = new Image("/images/Settings.png");
+        ImageView settingsButtonImageView = new ImageView(settingsButtonImage);
+        settingsButtonImageView.setFitHeight(100);
+        settingsButtonImageView.setFitHeight(100);
+        settingsButtonImageView.setPreserveRatio(true);
+        settingsButton.setGraphic(settingsButtonImageView);
     }
-    
+
 }
