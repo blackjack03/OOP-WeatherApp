a2a12cc40cff05f2ec24c7f1e93142f8958c7e67
Federico Panzavolta <federico.panzavolta@studio.unibo.it>
2025-01-14 10:23:09 +0100

    Modified interface ConfigManagerInterface.java

--- src/main/java/org/app/model/ConfigManagerInterface.java
+++ src/main/java/org/app/model/ConfigManagerInterface.java
@@ -1,11 +1,13 @@
 package org.app.model;
 
+import java.io.IOException;
+
 public interface ConfigManagerInterface {
 
-    void loadConfig(String filePath) throws Exception;
+    void loadConfig(String filePath) throws IOException;
 
-    AppConfig getConfig() throws Exception;
+    AppConfig getConfig();
 
-    void saveConfig(String filePath) throws Exception;
+    void saveConfig(String filePath) throws IOException;
 
 }
