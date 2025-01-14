b12e0b2ba909f226de7701a32b6ff4255c36226a
Federico Panzavolta <federico.panzavolta@studio.unibo.it>
2025-01-14 09:59:18 +0100

    Created ApiConfig.java and provided first implementation

---
+++ src/main/java/org/app/model/ApiConfig.java
@@ -0,0 +1,23 @@
+package org.app.model;
+
+public class ApiConfig {
+    private String baseUrl;
+    private String apiKey;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+    
+}
