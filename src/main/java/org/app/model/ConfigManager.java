856d3b027bbc6e906cefb2b796fa7f49cb414e17
Federico Panzavolta <federico.panzavolta@studio.unibo.it>
2025-01-14 10:21:38 +0100

    Created ConfigManager.java

---
+++ src/main/java/org/app/model/ConfigManager.java
@@ -0,0 +1,22 @@
+package org.app.model;
+
+import java.io.IOException;
+
+public class ConfigManager implements ConfigManagerInterface {
+    
+    @Override
+    public void loadConfig(String filePath) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public AppConfig getConfig() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void saveConfig(String filePath) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
