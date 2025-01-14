acae219191696ba61a8b4cdb31d8f56a1344f2fa
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-01-14 19:54:59 +0100

    Modified CheckpointWithMeteoImpl: added weatherReport field and provided implementation of getWeatherReport and getWeatherScore methods

--- src/main/java/org/app/travelmode/model/CheckpointWithMeteoImpl.java
+++ src/main/java/org/app/travelmode/model/CheckpointWithMeteoImpl.java
@@ -1,13 +1,23 @@
 package org.app.travelmode.model;
 
-import org.app.travelmode.directions.LatLng;
-
 import java.time.ZonedDateTime;
 
 public class CheckpointWithMeteoImpl extends CheckpointImpl implements CheckpointWithMeteo {
 
-    public CheckpointWithMeteoImpl(double latitude, double longitude, final ZonedDateTime arrivalDateTime) {
+    private final WeatherReport weatherReport;
+
+    public CheckpointWithMeteoImpl(double latitude, double longitude, final ZonedDateTime arrivalDateTime, final WeatherReport weatherReport) {
         super(latitude, longitude, arrivalDateTime);
+        this.weatherReport = weatherReport;
     }
 
+    @Override
+    public WeatherReport getWeatherReport() {
+        return this.weatherReport;
+    }
+
+    @Override
+    public int getWeatherScore() {
+        return this.weatherReport.calculateWeatherScore();
+    }
 }
