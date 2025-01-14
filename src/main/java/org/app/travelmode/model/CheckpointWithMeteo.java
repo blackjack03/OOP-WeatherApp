f442c39081bcfc1e4a12bf9b8d91928b0d0d8907
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-01-14 19:53:34 +0100

    Modified CheckpointWithMeteo: add getWeatherReport and getWeatherScore methods

--- src/main/java/org/app/travelmode/model/CheckpointWithMeteo.java
+++ src/main/java/org/app/travelmode/model/CheckpointWithMeteo.java
@@ -2,5 +2,16 @@ package org.app.travelmode.model;
 
 public interface CheckpointWithMeteo extends Checkpoint {
 
+    /**
+     * @return a {@link WeatherReport} for this checkpoint
+     */
+    WeatherReport getWeatherReport();
 
+    /**
+     * Returns a score between 0 and 100 calculated by combining the impact of each weather condition.
+     * A score of 100 represents ideal weather, while 0 represents the worst-case scenario.
+     *
+     * @return an integer between 0 and 100 representing this score.
+     */
+    int getWeatherScore();
 }
