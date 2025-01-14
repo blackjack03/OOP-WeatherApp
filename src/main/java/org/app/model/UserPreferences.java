86f3b57466eaf8ab15eb539cdaf5cf6cba285470
Federico Panzavolta <federico.panzavolta@studio.unibo.it>
2025-01-14 16:02:42 +0100

    Modified ApiConfig.java and UserPreferences.java for using jackson library

--- src/main/java/org/app/model/UserPreferences.java
+++ src/main/java/org/app/model/UserPreferences.java
@@ -1,30 +1,38 @@
 package org.app.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserPreferences {
     private String defaultCuty;
     private String units;
     private String language;
 
+    @JsonProperty
     public String getDefaultCuty() {
         return defaultCuty;
     }
 
+    @JsonProperty
     public void setDefaultCuty(String defaultCuty) {
         this.defaultCuty = defaultCuty;
     }
 
+    @JsonProperty
     public String getUnits() {
         return units;
     }
 
+    @JsonProperty
     public void setUnits(String units) {
         this.units = units;
     }
 
+    @JsonProperty
     public String getLanguage() {
         return language;
     }
 
+    @JsonProperty
     public void setLanguage(String language) {
         this.language = language;
     }
