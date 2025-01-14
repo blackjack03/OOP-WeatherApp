84356d4877f0fe128c3b7fa19c8e2a80cbd9ca1b
Federico Panzavolta <federico.panzavolta@studio.unibo.it>
2025-01-14 15:58:50 +0100

    provided first implementation for ConfigManager.java

--- src/main/java/org/app/model/ConfigManager.java
+++ src/main/java/org/app/model/ConfigManager.java
@@ -1,22 +1,50 @@
 package org.app.model;
 
+import java.io.File;
 import java.io.IOException;
 
-public class ConfigManager implements ConfigManagerInterface {
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConfigManager {
+    private static AppConfig config;
     
-    @Override
-    public void loadConfig(String filePath) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public static void loadConfig(String filePath) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            config = mapper.readValue(new File(filePath), AppConfig.class);
+            System.out.println ("Configuration loaded successfully.");
+        } catch (IOException e) {
+            throw new RuntimeException ("ERROR! File cannot be loaded.");
+        }
     }
 
-    @Override
-    public AppConfig getConfig() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public static AppConfig getConfig() {
+        if (config == null) {
+            throw new IllegalStateException ("Configuration not loaded; call loadConfig first. ");
+        }
+        return config;
     }
 
-    @Override
-    public void saveConfig(String filePath) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public static void saveConfig(String filePath) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filePath), config);
+            System.out.println ("Configuration saved successfully.");
+        } catch (IOException e) {
+            throw new RuntimeException ("ERROR! Save unsuccessful");
+        }
     }
-    
+
+    /*
+    // Esempio di utilizzo
+    public static void main(String[] args) {
+        loadConfig("src/main/java/org/files/config.json");
+        AppConfig appConfig = getConfig();
+
+        System.out.println("API URL: " + appConfig.getApi().getBaseUrl() + "API KEY: " + appConfig.getApi().getApiKey());
+        System.out.println("Default City: " + appConfig.getUserPreferences().getDefaultCuty());
+        System.out.println("Defalut Units: " + appConfig.getUserPreferences().getUnits());
+        System.out.println("Default Language: " + appConfig.getUserPreferences().getLanguage());
+    }
+    */
 }
