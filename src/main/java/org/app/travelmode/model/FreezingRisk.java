82b5610bb3ca7b7934aa96c1970b2d3fa23423a1
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-02-12 19:51:37 +0100

    Created FreezingRisk.java file and provided first implementation

---
+++ src/main/java/org/app/travelmode/model/FreezingRisk.java
@@ -0,0 +1,45 @@
+package org.app.travelmode.model;
+
+public class FreezingRisk extends AbstractWeatherCondition {
+
+    private static final double WEIGHT = 1.8;
+    private static final String NAME = "Freezing Risk";
+
+    public FreezingRisk(double freezingHeightMetres) {
+        super(NAME, FreezingLevel.fromValue(freezingHeightMetres).getIntensityScore());
+    }
+
+    @Override
+    public double getWeightedIntensityScore() {
+        return getIntensityScore() * WEIGHT;
+    }
+
+    private enum FreezingLevel {
+        NO_RISK(1500.0, 0),
+        LIGHT_RISK(1000.0, 20),
+        MODERATE_RISK(500.0, 40),
+        HEAVY_RISK(200, 70),
+        EXTREME_RISK(0, 100);
+
+        private final double intensity;
+        private final int intensityScore;
+
+        FreezingLevel(double intensity, int intensityScore) {
+            this.intensity = intensity;
+            this.intensityScore = intensityScore;
+        }
+
+        public static FreezingLevel fromValue(double value) {
+            for (final FreezingLevel freezingLevel : FreezingLevel.values()) {
+                if (value >= freezingLevel.intensity) {
+                    return freezingLevel;
+                }
+            }
+            return EXTREME_RISK;
+        }
+
+        public int getIntensityScore() {
+            return intensityScore;
+        }
+    }
+}
