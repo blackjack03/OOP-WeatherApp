de87d1744abc2cd3cc9db8a3ff9ea22ea520c7ff
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-05-19 23:19:15 +0200

    Removed an unused import

--- src/main/java/org/app/travelmode/MainTest.java
+++ src/main/java/org/app/travelmode/MainTest.java
@@ -2,7 +2,6 @@ package org.app.travelmode;
 
 import javafx.application.Application;
 import org.app.travelmode.controller.TravelModeControllerImpl;
-import org.app.travelmode.model.PlaceAutocompleteImpl;
 
 public class MainTest {
     public static void main(String[] args) {
