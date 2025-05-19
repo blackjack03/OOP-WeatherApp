9fdf9137db84169960e1ab726a2c2ba856f061a4
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-05-19 22:56:49 +0200

    Modified AbstractGoogleApiClient: executeRequest method has been transformed into requestJson method which now only takes care of getting the json file from the api

--- src/main/java/org/app/travelmode/model/AbstractGoogleApiClient.java
+++ src/main/java/org/app/travelmode/model/AbstractGoogleApiClient.java
@@ -1,10 +1,7 @@
 package org.app.travelmode.model;
 
-import com.google.gson.Gson;
 import org.app.model.AdvancedJsonReader;
 import org.app.model.AdvancedJsonReaderImpl;
-
-import java.util.Objects;
 
 public abstract class AbstractGoogleApiClient implements GoogleApiClient {
 
@@ -16,21 +13,17 @@ public abstract class AbstractGoogleApiClient implements GoogleApiClient {
         this.apiKey = apiKey;
     }
 
-    protected <T> T executeRequest(final String requestUrl, final Class<T> responseClass) {
+    protected String requestJson(final String requestUrl) {
         final AdvancedJsonReader jsonReader = new AdvancedJsonReaderImpl();
-        T response = null;
+        String rawJSon = null;
         try {
             jsonReader.requestJSON(requestUrl);
-            final String rawJSon = jsonReader.getRawJSON();
+            rawJSon = jsonReader.getRawJSON();
             System.out.println(rawJSon); //TODO: da eliminare
-
-            final Gson gson = new Gson();
-            response = gson.fromJson(rawJSon, responseClass);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Objects.requireNonNull(response, "La chiamata all'api ha dato una risposta vuota");
-        return response;
+        return rawJSon;
     }
 
     @Override
