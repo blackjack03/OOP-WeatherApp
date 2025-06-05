5d8184aeab417347b2f7e9c3800b3781197abb79
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-06-05 20:08:30 +0200

    Modified RouteAnalyzer and RouteAnalyzerImpl: added javadoc

--- src/main/java/org/app/travelmode/model/RouteAnalyzer.java
+++ src/main/java/org/app/travelmode/model/RouteAnalyzer.java
@@ -5,18 +5,34 @@ import org.app.travelmode.directions.SimpleDirectionsStep;
 
 import java.util.List;
 
+/**
+ * Defines a contract for analyzing routes and calculating
+ * intermediate points along a navigation path.
+ *
+ * <p>This analyzer supports:
+ * <ul>
+ *     <li>Route segmentation into intermediate points</li>
+ *     <li>Single-use processing pattern</li>
+ *     <li>Equidistant point distribution</li>
+ *     <li>Complex route analysis</li>
+ * </ul>
+ */
 public interface RouteAnalyzer {
 
     /**
-     * Calculates the intermediate points along the provided route. The distance between each intermediate point will be approximately equal.
+     * Calculates the intermediate points along the provided route.
+     * The distance between each intermediate point will be approximately equal.
      *
      * @param directionsRoute the route to analyze
-     * @return A list of intermediate points represented as {@link SimpleDirectionsStep} objects
+     * @return a list of intermediate points along the route, represented as {@link SimpleDirectionsStep} objects
      */
     List<SimpleDirectionsStep> calculateIntermediatePoints(DirectionsRoute directionsRoute);
 
     /**
-     * @return true if this {@link RouteAnalyzer} has already been consumed
+     * Checks if this analyzer instance has already processed a route.
+     *
+     * @return true if this {@link RouteAnalyzer} has already been consumed,
+     * false if it is still available for route analysis.
      */
     boolean isConsumed();
 
