4d7587b50f42a8e310093d8f42e64811942eb060
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-06-05 20:07:33 +0200

    Modified PolylineDecoder: added javadoc

--- src/main/java/org/app/travelmode/model/PolylineDecoder.java
+++ src/main/java/org/app/travelmode/model/PolylineDecoder.java
@@ -5,18 +5,31 @@ import org.app.travelmode.directions.LatLng;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class for decoding Google's encoded polyline format into a series of coordinates.
+ *
+ * <p>This decoder:
+ * <ul>
+ *     <li>Implements Google's polyline encoding algorithm</li>
+ *     <li>Converts a compressed string format to geographic coordinates</li>
+ *     <li>Handles latitude and longitude with 5 decimal places precision</li>
+ * </ul>
+ */
 public class PolylineDecoder {
 
-    public PolylineDecoder(){
+    /**
+     * Creates a new instance of PolylineDecoder.
+     */
+    public PolylineDecoder() {
     }
 
     /**
      * Decodes a polyline into a list of coordinates.
      *
-     * @param encodedPolyline The encoded polyline.
+     * @param encodedPolyline The Google-encoded polyline.
      * @return A list of {@link LatLng} representing the points on the polyline.
      */
-    public static List<LatLng> decode(final String encodedPolyline){
+    public static List<LatLng> decode(final String encodedPolyline) {
         final List<LatLng> polyline = new ArrayList<>();
         int index = 0;
         int len = encodedPolyline.length();
