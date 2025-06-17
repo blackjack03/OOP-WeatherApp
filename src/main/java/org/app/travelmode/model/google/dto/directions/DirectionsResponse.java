f24086038532a199d1218d19fade1a44035b9144
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-06-17 16:12:39 +0200

    Modified DirectionsResponse: added error_message field; added two constructors; added getErrorMessage method.

--- src/main/java/org/app/travelmode/model/google/dto/directions/DirectionsResponse.java
+++ src/main/java/org/app/travelmode/model/google/dto/directions/DirectionsResponse.java
@@ -1,13 +1,22 @@
 package org.app.travelmode.model.google.dto.directions;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DirectionsResponse {
 
-    private List<DirectionsRoute> routes;
-    private String status;
+    private final List<DirectionsRoute> routes;
+    private final String status;
+    private final String error_message;
 
-    public DirectionsResponse() {
+    public DirectionsResponse(final String status, final List<DirectionsRoute> routes, final String errorMessage) {
+        this.status = status;
+        this.routes = routes;
+        this.error_message = errorMessage;
+    }
+
+    public DirectionsResponse(final String status, final List<DirectionsRoute> routes) {
+        this(status, routes, null);
     }
 
     public List<DirectionsRoute> getRoutes() {
@@ -16,6 +25,10 @@ public class DirectionsResponse {
 
     public String getStatus() {
         return this.status;
+    }
+
+    public String getErrorMessage() {
+        return Objects.requireNonNullElse(this.error_message, "");
     }
 
     @Override
