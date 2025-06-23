0020aac343c1cc6cecdfa36b63b11cd9fbb03697
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-06-23 19:29:42 +0200

    DirectionsApiException, MapGenerationException and WeatherDataException classes have been created and first implementation has been provided.

---
+++ src/main/java/org/app/travelmode/model/exception/MapGenerationException.java
@@ -0,0 +1,26 @@
+package org.app.travelmode.model.exception;
+
+/**
+ * Exception thrown when errors occur during the static map generation process.
+ */
+public class MapGenerationException extends Exception {
+
+    /**
+     * Constructs a new map generation exception with the specified detail message.
+     *
+     * @param message the detail message describing the error condition
+     */
+    public MapGenerationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new map generation exception with the specified detail message and cause.
+     *
+     * @param message the detail message describing the error condition
+     * @param cause   the cause of this exception
+     */
+    public MapGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
