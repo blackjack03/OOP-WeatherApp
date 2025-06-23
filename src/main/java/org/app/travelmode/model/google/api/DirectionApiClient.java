54f56bc38adcd10d826efbb1c9a71515cb886f1c
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2025-06-23 19:45:39 +0200

    Modified DirectionApiClient and DirectionApiClientImpl: the getDirections method now throws a DirectionsApiException exception if the API call returns null, the call returns a status other than OK, or if an error occurs during the request.

--- src/main/java/org/app/travelmode/model/google/api/DirectionApiClient.java
+++ src/main/java/org/app/travelmode/model/google/api/DirectionApiClient.java
@@ -1,5 +1,6 @@
 package org.app.travelmode.model.google.api;
 
+import org.app.travelmode.model.exception.DirectionsApiException;
 import org.app.travelmode.model.google.dto.directions.DirectionsResponse;
 import org.app.travelmode.model.travel.api.TravelRequest;
 
@@ -10,6 +11,12 @@ public interface DirectionApiClient {
      *
      * @param travelRequest the request containing origin, destination, and timing information
      * @return a {@link DirectionsResponse} object containing the route information
+     * @throws DirectionsApiException if:
+     *                                <ul>
+     *                                   <li>The API returns a null response</li>
+     *                                    <li>The API returns a status other than "OK"</li>
+     *                                    <li>There's an error during the HTTP request</li>
+     *                                </ul>
      */
-    DirectionsResponse getDirections(TravelRequest travelRequest);
+    DirectionsResponse getDirections(TravelRequest travelRequest) throws DirectionsApiException;
 }
--- src/main/java/org/app/travelmode/model/google/impl/DirectionApiClientImpl.java
+++ src/main/java/org/app/travelmode/model/google/impl/DirectionApiClientImpl.java
@@ -1,12 +1,13 @@
 package org.app.travelmode.model.google.impl;
 
 import com.google.gson.Gson;
+import org.app.travelmode.model.exception.DirectionsApiException;
 import org.app.travelmode.model.google.dto.directions.DirectionsResponse;
 import org.app.travelmode.model.travel.api.TravelRequest;
 import org.app.travelmode.model.google.api.DirectionApiClient;
 import org.app.travelmode.model.google.api.GoogleApiRequestBuilder;
 
-import java.util.Objects;
+import java.io.IOException;
 
 /**
  * A client for interacting with Google Directions API.
@@ -36,33 +37,44 @@ public class DirectionApiClientImpl extends AbstractGoogleApiClient implements D
      *     <li>Parses the JSON response into a DirectionsResponse object</li>
      * </ul>
      *
-     * @param travelRequest the request containing origin, destination, and timing information
+     * @param travelRequest a {@link TravelRequest} object containing origin, destination,
+     *                      and departure time details
      * @return a {@link DirectionsResponse} object containing the route information
-     *
-     * @throws IllegalStateException if there is any problem executing the request to the Directions API.
-     * @throws NullPointerException if the call to the directions API returns null.
+     * @throws DirectionsApiException if:
+     *                                <ul>
+     *                                   <li>The API returns a null response</li>
+     *                                    <li>The API returns a status other than "OK"</li>
+     *                                    <li>There's an error during the HTTP request</li>
+     *                                </ul>
      */
     @Override
-    public DirectionsResponse getDirections(final TravelRequest travelRequest) throws IllegalStateException, NullPointerException {
-        final GoogleApiRequestBuilder requestBuilder = new GoogleApiRequestBuilderImpl(this.getBaseUrl(), this.getApiKey());
-        final String url = requestBuilder.addParameter("destination", "place_id:" + travelRequest.getArrivalLocationPlaceId())
-                .addParameter("origin", "place_id:" + travelRequest.getDepartureLocationPlaceId())
-                .addParameter("departure_time", String.valueOf(travelRequest.getDepartureDateTime().toEpochSecond()))
-                .addParameter("alternatives", "true")
-                .addParameter("language", "it")
-                .addParameter("units", "metric")
-                .build();
+    public DirectionsResponse getDirections(final TravelRequest travelRequest) throws DirectionsApiException {
+        try {
+            final GoogleApiRequestBuilder requestBuilder = new GoogleApiRequestBuilderImpl(this.getBaseUrl(), this.getApiKey());
+            final String url = requestBuilder.addParameter("destination", "place_id:" + travelRequest.getArrivalLocationPlaceId())
+                    .addParameter("origin", "place_id:" + travelRequest.getDepartureLocationPlaceId())
+                    .addParameter("departure_time", String.valueOf(travelRequest.getDepartureDateTime().toEpochSecond()))
+                    .addParameter("alternatives", "true")
+                    .addParameter("language", "it")
+                    .addParameter("units", "metric")
+                    .build();
 
-        final Gson gson = new Gson();
-        final DirectionsResponse directionsResponse = gson.fromJson(this.requestJson(url), DirectionsResponse.class);
+            final Gson gson = new Gson();
+            final DirectionsResponse directionsResponse = gson.fromJson(this.requestJson(url), DirectionsResponse.class);
 
-        if (!directionsResponse.getStatus().equals("OK")) {
-            throw new IllegalStateException(String.format("Errore nella ricerca del percorso: %s\n%s",
-                    directionsResponse.getStatus(), directionsResponse.getErrorMessage()));
+            if (directionsResponse == null) {
+                throw new DirectionsApiException("La chiamata all'API Directions ha restituito un risultato nullo.");
+            }
+
+            if (!directionsResponse.getStatus().equals("OK")) {
+                throw new DirectionsApiException(String.format("Errore nella ricerca del percorso: %s\n%s",
+                        directionsResponse.getStatus(), directionsResponse.getErrorMessage()));
+            }
+
+            return directionsResponse;
+        } catch (final IOException e) {
+            throw new DirectionsApiException("Errore durante la richiesta HTTP all'API Directions.", e);
         }
-
-        Objects.requireNonNull(directionsResponse, "La chiamata all'api directions ha restituito un risultato nullo.");
-        return directionsResponse;
     }
 
 }
