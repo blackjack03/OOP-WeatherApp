917c02d03e1eb3d093978b82c29179bbc1ac6542
Giacomo Casadei <giacomo.casadei03@gmail.com>
2025-07-02 12:41:08 +0200

    build.gradle updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ plugins {
     id("org.danilopianini.gradle-java-qa") version "1.70.0"
 
     id("application");
-    id("org.openjfx.javafxplugin") version "0.0.14"
 }
 
 repositories { // Where to search for dependencies
@@ -36,10 +35,6 @@ dependencies {
         }
     }
 
-    // Maven dependencies are composed by a group name, a name and a version, separated by colons
-    implementation("com.omertron:API-OMDB:1.5")
-    implementation("org.jooq:jool:0.9.15")
-
     // Import libraries
     implementation("org.jsoup:jsoup:1.18.1")
     implementation("com.google.code.gson:gson:2.8.6")
@@ -47,20 +42,12 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.15.2")
 
-    /*
-     * Simple Logging Facade for Java (SLF4J) with Apache Log4j
-     * See: http://www.slf4j.org/
-     */
-    val slf4jVersion = "2.0.16"
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    // Logback backend for SLF4J
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.11")
-
     // JUnit API and testing engine
     val jUnitVersion = "5.11.3"
     // when dependencies share the same version, grouping in a val helps to keep them in sync
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+
     testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
 }
 
